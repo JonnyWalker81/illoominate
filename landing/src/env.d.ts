26b1bc8b9cf64e0c03522d587939fd66638749c8
Jonathan Rothberg <jon@join.build>
2026-01-13 18:39:30 -0800

    Update Astro

---
+++ landing/src/env.d.ts
@@ -0,0 +1,19 @@
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
+
+interface Env {
+  DB: D1Database;
+  TURNSTILE_SECRET_KEY: string;
+  RESEND_API_KEY: string;
+  RESEND_AUDIENCE_ID: string;
+  RESEND_WEBHOOK_SECRET: string;
+  VERIFICATION_BASE_URL: string;
+  ADMIN_EMAIL: string;
+  FROM_EMAIL: string;
+}
+
+declare namespace App {
+  interface Locals extends Runtime {}
+}
